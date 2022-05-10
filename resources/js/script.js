073936e09433c3e123f74307da2d582de3ded628
thiripwint645 <thiripwint21@gmail.com>
2022-05-10 22:11:06 +0630

    team

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,3 +13,37 @@ $(window).on("load", function() {
     $("#preloader").fadeOut(1000);
     $("#status").delay(1100).fadeOut(1000);
 });
+// Carousel
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+
+            1000: {
+                items: 2,
+            },
+        },
+    });
+});
+// Progress Bar
+$(document).ready(function() {
+    $("#progress-elements").waypoint(
+        function() {
+            $(".progress-bar").each(function() {
+                $(this).animate({
+                        width: $(this).attr("aria-valuenow") + "%",
+                    },
+                    3000
+                );
+            });
+            this.destroy();
+        }, {
+            offset: "bottom-in-view",
+        }
+    );
+});
